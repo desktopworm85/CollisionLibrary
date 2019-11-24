088b454dcef016da09aa82453cce9e0f10ffca81
desktopworm85 <39500730+desktopworm85@users.noreply.github.com>
2019-11-24 22:06:20 +0000

    Update p5.collision.js

--- p5.collision.js
+++ p5.collision.js
@@ -1,11 +1,11 @@
-p5.prototype.pointpoint = function(x1,y1,x2,y2) {
+p5.prototype.pointPoint = function(x1,y1,x2,y2) {
 	if (x1 == x2 && y1 == y2) {
 		return true;
 	}
 	return false;
 }
 
-p5.prototype.pointcircle = function(px,py,cx,cy,r) {
+p5.prototype.pointCircle = function(px,py,cx,cy,r) {
 	float distx = px - cx;
 	float disty = py - cy;
 	float distance = sqrt((distx*distx) + (disty*disty));
@@ -16,7 +16,7 @@ p5.prototype.pointcircle = function(px,py,cx,cy,r) {
 	return false;
 }
 
-p5.prototype.circlecircle = function(c1x,c1y,c1r,c2x,c2y,c2r) {
+p5.prototype.circleCircle = function(c1x,c1y,c1r,c2x,c2y,c2r) {
 	float distx = c1x - c2x;
 	float disty = c1y - c2y;
 	float distance = sqrt((distx*distx) + (disty*disty));
@@ -27,21 +27,21 @@ p5.prototype.circlecircle = function(c1x,c1y,c1r,c2x,c2y,c2r) {
 	return false;
 }
 
-p5.prototype.pointrect = function(px,py,rx,ry,rw,rh) {
+p5.prototype.pointRect = function(px,py,rx,ry,rw,rh) {
 	if (px >= rx && px <= rx + rw && py >= ry && py <= ry + rh) {
 		return true;
 	}
 	return false;
 }
 
-p5.prototype.rectrect = function(r1x,r1y,r1w,r1h,r2x,r2y,r2w,r2h) {
+p5.prototype.rectRect = function(r1x,r1y,r1w,r1h,r2x,r2y,r2w,r2h) {
 	if (r1x + r1w >= r2x && r1x <= r2x + r2w && r1y + r1h >= r2y && r1y <= r2y + r2h) {
 		return true;
 	}
 	return false;
 }
 
-p5.prototype.circlerect = function(cx,cy,r,rx,ry,rw,rh) {
+p5.prototype.circleRect = function(cx,cy,r,rx,ry,rw,rh) {
 	float testx = cx;
 	float testy = cy;
 	
